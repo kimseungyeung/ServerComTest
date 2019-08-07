0a3832869418f30f9cceeef4cd562fb6d9be43a0
kimseungyeung <seungyeung1@naver.com>
2019-08-07 18:28:18 +0900

    파이어베이스 기능추가

--- app/src/main/java/com/server/servercomtest/Data/UserData.java
+++ app/src/main/java/com/server/servercomtest/Data/UserData.java
@@ -1,11 +1,13 @@
 package com.server.servercomtest.Data;
 
 import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import lombok.Data;
 
 @Data
-public class UserData {
+public class UserData implements Parcelable {
     String username;
     String password;
     String realname;
@@ -19,7 +21,40 @@ public class UserData {
         this.mobile=p;
 
     }
+    public UserData(Parcel p){
+        this.username=p.readString();
+        this.password=p.readString();
+        this.realname=p.readString();
+        this.email=p.readString();
+        this.mobile=p.readString();
+    }
     public UserData(){
 
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.email);
+        dest.writeString("");
+        dest.writeString(this.realname);
+        dest.writeString(this.email);
+        dest.writeString(this.mobile);
+    }
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
+        @Override
+        public Object createFromParcel(Parcel source) {
+            return new UserData(source);
+        }
+
+        @Override
+        public Object[] newArray(int size) {
+            return new UserData[size];
+        }
+    };
+
 }
